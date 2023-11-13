139ae953d2078ca3ff3ce403ac174efd6893bb3d
Kyle Foral <kyleforal@gmail.com>
2023-11-12 21:27:18 -0800

    submit button working for form with alert

---
+++ all-working.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('#contact-form');
+const firstName = document.querySelector('#first-name');
+form.addEventListener("submit", function() {
+  alert("Form Submitted");
+});
